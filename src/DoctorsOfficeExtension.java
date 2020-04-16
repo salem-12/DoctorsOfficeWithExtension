7a29d5193d4ec8fd79847a72e10557a3cfe8eb73
salem-12 <salsalem02>
2020-04-16 18:33:30 -0400

    First commit

---
+++ src/DoctorsOfficeExtension.java
@@ -0,0 +1,15 @@
+public class DoctorsOfficeExtension {
+    public static void main(String[] args) {
+
+        String patientName="Stacey Manson";
+        boolean hasInsurance=false;
+        double bodytemp=102;
+         int numVisits= 5;
+        System.out.println("patientsName: "+patientName);
+        System.out.println("Patient has insurance: "+hasInsurance);
+        System.out.println("Body temperature: "+bodytemp);
+        System.out.println("Number of visits: "+numVisits);
+
+
+    }
+}
